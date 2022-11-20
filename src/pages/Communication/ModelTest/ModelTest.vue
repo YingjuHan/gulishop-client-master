7fbf51518ee1f9f35cf36b09f43c79a79d372322
YingjuHan <hanyingju@live.cn>
2022-11-20 12:45:04 +0800

    004, 005

--- src/pages/Communication/ModelTest/ModelTest.vue
+++ src/pages/Communication/ModelTest/ModelTest.vue
@@ -2,9 +2,16 @@
   <div>
     <h2>深入v-model</h2>
 
-    <input type="text">
-    <span>xxx</span>
+    <input type="text" v-model="msg">
+    <span>{{ msg }}</span>
     <br>
+
+    <hr/>
+    <!-- 下面的msg属于props，用于父子组件通信 -->
+    <!-- @input不是原生DOM的input事件，属于自定义事件 -->
+    <!-- <CustomInput :value="msg" @input="msg = $event"></CustomInput> -->
+    <!-- 简化写法 -->
+    <CustomInput v-model="msg"></CustomInput>
   </div>
 </template>
 
@@ -14,6 +21,11 @@
     name: 'ModelTest',
     components: {
       CustomInput
+    },
+    data() {
+      return {
+        msg: '我爱你'
+      }
     }
   }
 </script>
