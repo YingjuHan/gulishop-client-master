939a90139102702ad5c49695d19b969d503fa857
YingjuHan <hanyingju@live.cn>
2022-11-20 20:21:38 +0800

    remove files, create empty project, copy header html and header style to Header.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <template>
   <div id="app">
+    <h1> HELLO </h1>
     <Header></Header>
-    <router-view></router-view>
-    <Footer v-show="$route.path !=='/login' && $route.path !== '/register' "></Footer>
-    <!-- <Footer v-show="!$route.meta.isShowFooter"></Footer> -->
+
   </div>
 </template>
 
 <script>
-import Header from '@/components/Header'
-import Footer from '@/components/Footer'
+import Header from '@/components/Header';
+
 export default {
   name: 'App',
 
   components: {
     Header,
-    Footer
   },
-  mounted() {
-    this.getCategoryList();
-  },
-  methods: {
-    getCategoryList() {
-      this.$store.dispatch('getCategoryList')
-    }
-  }
+
 }
 </script>
 
--- src/pages/Communication/EventTest/Event2.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div style="background: #ccc; height: 80px;">
-    <h2>Event2组件</h2>
-    <button @click="$emit('click', '自定义事件click')">分发自定义click事件</button><br>
-    <button @click="$emit('xxx', '自定义事件xxx')">分发自定义xxx事件</button><br>
-  </div>
-</template>
