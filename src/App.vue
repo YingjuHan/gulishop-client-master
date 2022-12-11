db21a7304c45f7127adaa8c63bcf47929967259f
HanYingju <hanyingju@live.cn>
2022-12-11 16:52:43 +0800

    005-011

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <h1> HELLO </h1>
     <Header></Header>
-
+    <router-view></router-view> <!-- 路由组件出口的地方 -->
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header';
+import Footer  from '@/components/Footer';
 
 export default {
   name: 'App',
 
   components: {
     Header,
+    Footer,
   },
 
 }
