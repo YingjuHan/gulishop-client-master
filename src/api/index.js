056467dd4b05641477b6b162782abbf5f0cb1082
HanYingju <hanyingju@live.cn>
2022-12-25 16:12:12 +0800

    三级联动接口，进度条样式

---
+++ src/api/index.js
@@ -0,0 +1,9 @@
+// 使用这个模块：API同意进行管理
+import request from "./request";
+
+// 三级联动的接口
+// /api/product/getBaseCategoryList get类型 无参数
+
+export const reqCategoryList = () => {
+    return request({ url: '/product/getBaseCategoryList', method: 'get' }); // 发请求：axios发请求返回Promise对象
+}
--- src/api/request.js
+++ src/api/request.js
@@ -1,5 +1,11 @@
-import axios from 'axios';
 // 对axios二次封装
+import axios from 'axios';
+
+// 引入进度条
+import nprogress from 'nprogress';
+// 引入进度条样式
+import 'nprogress/nprogress.css';
+// start 进度条开始   done 进度条结束
 
 // 1、利用axios对象的方法create,创建一个axios实例
 // 2、request就是axios（配置后的）
@@ -12,6 +18,7 @@ const request = axios.create({
 // 请求拦截器：再发请求之前，请求拦截器可以检测到，可以在请求发出去之前做一些事情
 request.interceptors.request.use((config) => {
   // config：配置对象，对象中有一个属性是header请求头
+  nprogress.start();
   return config;
 });
 
@@ -19,6 +26,7 @@ request.interceptors.request.use((config) => {
 request.interceptors.response.use(
   (res) => {
     // 成功的回调函数：服务器响应数据回来以后，响应拦截器可以监测到，可以做一些事情
+    nprogress.done();
     return res.data;
   },
   (error) => {
