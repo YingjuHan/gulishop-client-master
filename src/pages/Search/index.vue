8ec615b971ec79780108f1950738333cbac49dc9
YingjuHan <hanyingju@live.cn>
2023-03-29 10:24:23 +0800

    052-分页器静态组件

--- src/pages/Search/index.vue
+++ src/pages/Search/index.vue
@@ -77,36 +77,10 @@
               </li>
             </ul>
           </div>
-          <div class="fr page">
-            <div class="sui-pagination clearfix">
-              <ul>
-                <li class="prev disabled">
-                  <a href="#">«上一页</a>
-                </li>
-                <li class="active">
-                  <a href="#">1</a>
-                </li>
-                <li>
-                  <a href="#">2</a>
-                </li>
-                <li>
-                  <a href="#">3</a>
-                </li>
-                <li>
-                  <a href="#">4</a>
-                </li>
-                <li>
-                  <a href="#">5</a>
-                </li>
-                <li class="dotted"><span>...</span></li>
-                <li class="next">
-                  <a href="#">下一页»</a>
-                </li>
-              </ul>
-              <div><span>共10页&nbsp;</span></div>
-            </div>
-          </div>
+          
         </div>
+        <!-- 分页器 -->
+        <Pagination/>
       </div>
     </div>
   </div>
@@ -119,8 +93,8 @@ export default {
   name: 'Search',
 
   components: {
-    SearchSelector
-  },
+    SearchSelector,
+},
   data() {
     return {
       searchParams: { // 带给服务器的参数
